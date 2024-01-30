c258e165f7fd514d73d96f1cc15de86a281e785c
jaesekim <jaeseki@gmail.com>
2024-01-30 16:47:22 +0900

    gitignore 수정

---
+++ TVShowMediaProject/TVShowMediaProject/Network/APIManager.swift
@@ -0,0 +1,8 @@
+//
+//  APIManager.swift
+//  TVShowMediaProject
+//
+//  Created by 김재석 on 1/30/24.
+//
+
+import Foundation
---
+++ TVShowMediaProject/TVShowMediaProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TVShowMediaProject
+//
+//  Created by 김재석 on 1/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
